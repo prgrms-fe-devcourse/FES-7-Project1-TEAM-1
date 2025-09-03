ea0baff315f0bb45cec964d820c7436fab2fccf3
suyeon <suyeon1104@gmail.com>
2025-09-03 15:35:09 +0900

    [FIX] 1차  마무리. 루트 문서 목록, 루트 문서 추가, 문서 삭제 구현

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,92 +1,110 @@
-
-const BASE_URL = `https://kdt-api.fe.dev-cos.com/documents`
+const BASE_URL = `https://kdt-api.fe.dev-cos.com/documents`;
 
 const DEFAULT_HEADER = {
-    'x-username': 'strangers', // 팀 고유 헤더
-    'Content-Type': 'application/json'
-}
+  "x-username": "strangers", // 팀 고유 헤더
+  "Content-Type": "application/json",
+};
 
 const APIS = {
-    // 전체 문서 조회
-    async getDocument() {
-        return fetch(BASE_URL, { headers: DEFAULT_HEADER })
-            .then(async (res) => {
-                if (!res.ok) throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
-                const data = await res.json();
-                // console.log(data);
-                return data;
-            })
-            .catch((err) => {
-                console.error(err);
-                return [];
-            });
-    },
-    // 특정 문서 조회
-    async getSpecificDocument(documentId) {
-        return fetch(`${BASE_URL}/${documentId}`, {
-            headers: DEFAULT_HEADER,
-            method: "GET",
-        })
-            .then(async (res) => {
-                if (!res.ok) throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
-                const data = await res.json();
-                // console.log(data);
-                return data;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    },
-    // 문서 추가
-    async addDocument(data) {
-        return fetch(BASE_URL, {
-            headers: DEFAULT_HEADER,
-            method: "POST",
-            body: JSON.stringify(data)
-        })
-            .then(async (res) => {
-                if (!res.ok) throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
-                const data = await res.json();
-                // console.log(data);
-                return data;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    },
-    // 문서 수정
-    async updateDocument(documentId, data) {
-        return fetch(`${BASE_URL}/${documentId}`, {
-            headers: DEFAULT_HEADER,
-            method: "PUT",
-            body: JSON.stringify(data)
-        })
-            .then(async (res) => {
-                if (!res.ok) throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
-                const data = await res.json();
-                // console.log(data);
-                return data;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    },
-    // 문서 삭제
-    async deleteDocument(documentId) {
-        return fetch(`${BASE_URL}/${documentId}`, {
-            headers: DEFAULT_HEADER,
-            method: "DELETE"
-        })
-            .then(async (res) => {
-                if (!res.ok) throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
-                const data = await res.json();
-                // console.log(data);
-                return data;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    },
-}
+  // 전체 문서 조회
+  async getDocument() {
+    return fetch(BASE_URL, { headers: DEFAULT_HEADER })
+      .then(async (res) => {
+        if (!res.ok)
+          throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
+        const data = await res.json();
+        // console.log(data);
+        return data;
+      })
+      .catch((err) => {
+        console.error(err);
+        return [];
+      });
+  },
+  // 특정 문서 조회
+  async getSpecificDocument(documentId) {
+    return fetch(`${BASE_URL}/${documentId}`, {
+      headers: DEFAULT_HEADER,
+      method: "GET",
+    })
+      .then(async (res) => {
+        if (!res.ok)
+          throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
+        const data = await res.json();
+        // console.log(data);
+        return data;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  // 문서 추가
+  async addDocument(data) {
+    return fetch(BASE_URL, {
+      headers: DEFAULT_HEADER,
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then(async (res) => {
+        if (!res.ok)
+          throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
+        const data = await res.json();
+        // console.log(data);
+        return data;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  // 문서 수정
+  async updateDocument(documentId, data) {
+    return fetch(`${BASE_URL}/${documentId}`, {
+      headers: DEFAULT_HEADER,
+      method: "PUT",
+      body: JSON.stringify(data),
+    })
+      .then(async (res) => {
+        if (!res.ok)
+          throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
+        const data = await res.json();
+        // console.log(data);
+        return data;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  // 문서 삭제
+  async deleteDocument(documentId) {
+    return fetch(`${BASE_URL}/${documentId}`, {
+      headers: DEFAULT_HEADER,
+      method: "DELETE",
+    })
+      .then(async (res) => {
+        if (!res.ok)
+          throw new Error(`HTTP ERROR ${res.status} - ${res.statusText}`);
+        const data = await res.json();
+        // console.log(data);
+        return data;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  open(doc) {
+    const title = document.querySelector("#editor-title");
+    title.innerText = doc.title ?? "";
+
+    const noteEditor = document.querySelector("#noteEditor");
+    noteEditor.value = doc.content ?? "";
+  },
+  // 트리뷰가 갱신될 필요가 있을 때마다 호출
+  notifyUpdate(doc) {
+    APIS.updateCallback(doc);
+  },
+  registerUpdate(callback) {
+    APIS.updateCallback = callback;
+  },
+};
 
 export default APIS;
--- src/script/explorer.js
+++ src/script/explorer.js
@@ -1,21 +1,70 @@
 import APIS from "../modules/api.js";
 
+// 
+
+class DataModel {
+    #docList;
+    
+    async listRoots() {
+        this.#docList = await APIS.getDocument();
+        console.log("독리스트", this.#docList);
+    }
+
+    async deleteDocument(docId) {
+        for (const idx in this.#docList) {
+            if (this.#docList[idx].id == docId) {
+                delete this.#docList[idx];
+            }
+        }
+        await APIS.deleteDocument(docId);
+    }
+    async createDocument() {
+        const doc = await APIS.addDocument({
+            "title": "새 페이지",
+            "content": "",
+            "parent": null
+          })
+          this.#docList.push(doc);
+        return doc;
+    }
+
+    async updateDocument(doc) {
+        const updatedDoc = await APIS.updateDocument(doc.id, doc);
+        await this.listRoots();
+        render();
+        return updatedDoc;
+    }
+
+    forEachRoot(func) {
+        this.#docList.forEach(func);
+    }
+
+    getDocument(docId) {
+        return APIS.getSpecificDocument(docId);
+    }
+};
+
+let dataModel = new DataModel();
+
+function render() {
+    try {
+        let personal_page = document.querySelector("#personal-page");
+        personal_page.innerHTML = "";
+        // 현재 팀 Document 항목들의 title 출력
+        dataModel.forEachRoot(doc => {
+            personal_page.appendChild(createNode(doc));
+
+        });
+    } catch (err) {
+        console.error(err)
+    }
+}
+
 // 문서 로드되었을 때
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        let sidebar = document.querySelector(".sidebar-nav");
-        const documentTitle = await APIS.getDocument();
-
-        // 현재 팀 Document 항목들의 title 출력
-        documentTitle.forEach(element => {
-            console.log(element.title)
-            const docNode = document.createElement("a");
-            docNode.textContent = element.title;
-            docNode.classList.add("nav-link");
-            // docNode.classList.add("active");
-            sidebar.appendChild(docNode);
-
-        });
+        await dataModel.listRoots();
+        render();
     } catch (err) {
         console.error(err)
     }
@@ -26,21 +75,63 @@ document.addEventListener("DOMContentLoaded", async () => {
 const addButton = document.querySelector("#addNode");
 addButton.addEventListener("click", async() => {
     try {
-        console.log("addButton");
-        let sidebar = document.querySelector(".sidebar-nav");
-        const data = await APIS.addDocument({
-            "title": "문서3",
-            "content": "문서3의 내용입니당",
-            "parent": null
-          })
-        const docNode = document.createElement("a");
-        docNode.textContent = data.title;
-        docNode.classList.add("nav-link");
-        
-        sidebar.appendChild(docNode);
+        let personal_page = document.querySelector("#personal-page");
+        const doc = await dataModel.createDocument();
+        personal_page.appendChild(createNode(doc));
+        APIS.open(doc);
     } catch (err) {
         console.error(err)
     }
 });
+
+
+// 문서 보기, 문서 삭제, 하위 문서 추가, 이름 변경 이벤트 처리
+let personal_page = document.querySelector("#personal-page");
+personal_page.addEventListener("click", async (ev) => {
+    try {
+        // console.log(ev.target.parentElement.innerText);
+        // console.log(ev.target.parentElement.id);
+        // console.log("아이디", ev.target.id);
+        if (ev.target.id === "addDocButton") {
+
+        } else if (ev.target.id === "deleteDocButton") {
+            let docId = ev.target.parentElement.id;
+            await dataModel.deleteDocument(docId);
+            render();
+        } else {
+            let docId = ev.target.id;
+            let doc = await dataModel.getDocument(docId);
+            APIS.open(doc);
+        }
+    } catch (err) {
+        console.error(err)
+    }
+});
+
+// 트리뷰가 갱신될 때마다 호출해야 하는 함수
+function updateList(doc) {
+    console.log("updateList:", doc);
+}
+
+APIS.registerUpdate(updateList);
+
+function createNode(doc) {
+    const docNode = document.createElement("a");
+    docNode.classList.add("nav-link");
+    const div = document.createElement("div");
+    div.textContent = doc.title;
+    div.setAttribute("id", doc.id);
+    docNode.appendChild(div);
+    const addButton = document.createElement("button");
+    addButton.textContent = "+";
+    addButton.setAttribute("id", "addDocButton");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "-";
+    deleteButton.setAttribute("id", "deleteDocButton");
+    div.appendChild(addButton);
+    div.appendChild(deleteButton);
+
+    return docNode;
+}
 
 // 폴더 접기/펼치기
